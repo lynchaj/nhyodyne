9ded00c33e673b24732b3ab1e93d1fbf654667fd
lynchaj <86925539+lynchaj@users.noreply.github.com>
2021-12-27 08:05:23 -0500

    Update buildMBC.bat

--- Z80MBC/TestPrograms/buildMBC.bat
+++ Z80MBC/TestPrograms/buildMBC.bat
@@ -1,21 +1,21 @@
-TASM -t80 -b halt.asm MBC\halt.bin
-TASM -t80 -b jploop.asm MBC\jploop.bin
-TASM -t80 -b jrloop.asm MBC\jrloop.bin
+TASM -t80 -b halt.asm halt.bin
+TASM -t80 -b jploop.asm jploop.bin
+TASM -t80 -b jrloop.asm jrloop.bin
 pause
-TASM -t80 -b tone.asm MBC\tone.bin
-TASM -t80 -b pingmpcl.asm MBC\pingmpcl.bin
-TASM -t80 -b scream.asm MBC\scream.bin
-TASM -t80 -b scream1.asm MBC\scream1.bin
-TASM -t80 -b scream2.asm MBC\scream2.bin
-TASM -t80 -b scream3.asm MBC\scream3.bin
+TASM -t80 -b tone.asm tone.bin
+TASM -t80 -b pingmpcl.asm pingmpcl.bin
+TASM -t80 -b scream.asm scream.bin
+TASM -t80 -b scream1.asm scream1.bin
+TASM -t80 -b scream2.asm scream2.bin
+TASM -t80 -b scream3.asm scream3.bin
 pause
-TASM -t80 -b typewrit.asm MBC\typewrit.bin
-TASM -t80 -b pingrtc.asm MBC\pingrtc.bin
-TASM -t80 -b pinguart.asm MBC\pinguart.bin
-TASM -t80 -b inuart.asm MBC\inuart.bin
+TASM -t80 -b typewrit.asm typewrit.bin
+TASM -t80 -b pingrtc.asm pingrtc.bin
+TASM -t80 -b pinguart.asm pinguart.bin
+TASM -t80 -b inuart.asm inuart.bin
 pause
-TASM -t80 -b leds.asm MBC\leds.bin
-TASM -t80 -b button.asm MBC\button.bin
-TASM -t80 -b button1.asm MBC\button1.bin
-TASM -t80 -b tone2.asm MBC\tone2.bin
+TASM -t80 -b leds.asm leds.bin
+TASM -t80 -b button.asm button.bin
+TASM -t80 -b button1.asm button1.bin
+TASM -t80 -b tone2.asm tone2.bin
 pause
