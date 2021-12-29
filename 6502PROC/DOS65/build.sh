9359c2c51a16e5dd27acdf17c9d0f557b42376b3
Dan Werner <danwerner21@gmail.com>
2021-12-29 15:28:12 -0600

    6502 BOM update

--- 6502PROC/DOS65/build.sh
+++ 6502PROC/DOS65/build.sh
@@ -4,12 +4,12 @@ srec_cat  DOS65.out -binary -offset 0x0100 -exclude 0x0000 0xB800 -o  DOS65.s -d
 srec_cat  DOS65.out -binary -offset 0x0100 -exclude 0x0000 0xB7F3 -exclude 0xE500 0xFFFF -o  DOS65.s19  -data-only
 srec_cat  DOS65.s  -offset -0x8000  -o  DOS651.s  -data-only
 srec_cat  DOS651.s -exclude 0x6000 0x7EE0  -o DOS65.hex  -intel -address-length=2
-srec_cat  DOS65.out -binary -exclude 0x0000 0xB7E0 -o  DOS65A.s  -data-only
+srec_cat  DOS65.out -binary -offset 0x0100 -exclude 0x0000 0xB7E0 -o  DOS65A.s  -data-only
 srec_cat  DOS65A.s  -offset -0xB7E0  -o  DOS652.s  -data-only
 srec_cat  DOS652.s -o DOS65.COM  -BINARY
-rm DOS65.s
-rm DOS65A.s
-rm DOS651.s
-rm DOS652.s
-rm DOS65.out
-rm DOS65.o
+;rm DOS65.s
+;rm DOS65A.s
+;rm DOS651.s
+;rm DOS652.s
+;rm DOS65.out
+;rm DOS65.o
