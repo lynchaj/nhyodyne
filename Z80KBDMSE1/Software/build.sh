34cccff68882e13691fcacc546b23f23a2e535bd
Wayne Warthen <wwarthen@gmail.com>
2022-01-07 16:03:37 -0800

    Add ps2info
    - Replaced kbdinfo with ps2info

--- Z80KBDMSE1/Software/build.sh
+++ Z80KBDMSE1/Software/build.sh
@@ -2,9 +2,9 @@
 #srec_cat kbdtest.bin -binary -offset 0x0100 --address-length=2 -o kbdtest.hex -Intel
 cat kbdtest.bin > kbdtest.com
 
-~/RomWBW-dev/Tools/unix/uz80as/uz80as -t z80 kbdinfo.asm kbdinfo.bin
+~/RomWBW-dev/Tools/unix/uz80as/uz80as -t z80 ps2info.asm ps2info.bin
 #srec_cat kbdinfo.bin -binary -offset 0x0100 --address-length=2 -o kbdinfo.hex -Intel
-cat kbdinfo.bin > kbdinfo.com
+cat ps2info.bin > ps2info.com
 
 ~/RomWBW-dev/Tools/unix/uz80as/uz80as -t z80 mseinfo.asm mseinfo.bin
 #srec_cat mseinfo.bin -binary -offset 0x0100 --address-length=2 -o mseinfo.hex -Intel
